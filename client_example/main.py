e9175deb8d89c51b3f4ea98ff05fef4419eec8a7
Jamie Chang <jamie.cheng.chang@gmail.com>
2019-01-31 10:27:51 +0000

    Update client example to demonstrate streaming interaction

--- client_example/main.py
+++ client_example/main.py
@@ -10,8 +10,16 @@ async def main():
     # perform request
     channel = Channel('127.0.0.1', 50051, loop=loop)
     stub = CheckerStub(channel)
-    response = await stub.check_weather(WeatherRequest())
+    response = await stub.check_weather(WeatherRequest(end=False))
     print(f"Weather is now {response.temperature}")
+
+    async with stub.stream_weather.open() as stream:
+        await stream.send_message(WeatherRequest(end=False))
+        for i in range(10):
+            message = await stream.recv_message()
+            print(f"Weather is now {message.temperature}")
+        await stream.send_message(WeatherRequest(end=True))
+
     channel.close()
 
 
