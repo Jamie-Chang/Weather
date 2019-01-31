bfad87f7750351d7a586d6b5b453fe59f4b3c1bd
Jamie Chang <jamie.cheng.chang@gmail.com>
2019-01-31 10:24:26 +0000

    Add streaming capabilities to the service.

--- weather_service/darksky.py
+++ weather_service/darksky.py
@@ -1,3 +1,4 @@
+"""Module that talks to darksky API."""
 import aiohttp
 import asyncio
 import os
@@ -7,11 +8,18 @@ API_KEY = os.environ['DARK_SKY_API_KEY']
 LOCATION = '51.5074,0.1278'
 
 
-def get_url():
+def _get_url():
     return f'https://api.darksky.net/forecast/{API_KEY}/{LOCATION}?units=si'
 
 
 async def fetch():
+    """Fetch the darksky JSON doc at the moment."""
     async with aiohttp.ClientSession() as session:
         async with session.get(get_url()) as response:
             return await response.json()
+
+
+async def fetch_temperature() -> float:
+    """Fetch the current temperature from darksky."""
+    temperature_doc = await fetch()
+    return temperature_doc.get('currently', {}).get('temperature')
--- weather_service/server.py
+++ weather_service/server.py
@@ -1,3 +1,4 @@
+"""gRPC service."""
 import asyncio
 import darksky
 
@@ -8,16 +9,40 @@ from proto.weather_grpc import CheckerBase
 from proto.weather_pb2 import WeatherReply
 
 
-
 class Checker(CheckerBase):
+    """gRPC service to check the weather."""
+
     async def check_weather(self, stream):
+        """Check the current weather."""
         request = await stream.recv_message()
-        temperature_doc = await darksky.fetch()
-        temperature = temperature_doc.get('currently', {}).get('temperature')
+        temperature = await get_temperature()
         await stream.send_message(WeatherReply(temperature=temperature))
+
+    async def stream_weather(self, stream):
+        """Stream the current weather.
+
+        This is a stream - stream gRPC.
+        """
+        request = await stream.recv_message()
+        stopped = False
+
+        async def do_stream():
+            while not stopped:
+                temperature = await darksky.fetch_temperature()
+                await stream.send_message(
+                    WeatherReply(temperature=temperature)
+                )
+                await asyncio.sleep(2)
+
+        task = asyncio.create_task(do_stream())
+        while not stopped:
+            stopped = (await stream.recv_message()).end
+
+        await task
 
 
 async def main():
+    """Entry point."""
     # start server
     server = Server([Checker()], loop=asyncio.get_event_loop())
     await server.start('127.0.0.1', 50051)
